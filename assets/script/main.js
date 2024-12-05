c8e514cf4cde57a8fc1046795bff2a6a94e8591e
Ahmed Samy <as8609377@gmail.com>
2024-12-05 08:02:30 +0200

    The front side is finished at the top of the page.

---
+++ assets/script/main.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", () => {
+  const heroImage = document.querySelector(".hero img");
+  heroImage.classList.add("animate");
+});
+
+window.addEventListener("load", () => {
+  const enjoyText = document.querySelector(".enjoy h1");
+  enjoyText.classList.add("animate");
+});
+window.addEventListener("load", () => {
+  const enjoyText = document.querySelector(".decore img");
+  enjoyText.classList.add("animate");
+});
