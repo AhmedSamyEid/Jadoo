961457a0a04857cf245a75d4dd20a0e83982a161
Ahmed Samy <as8609377@gmail.com>
2024-12-07 00:43:18 +0200

    footer is finished

--- assets/script/main.js
+++ assets/script/main.js
@@ -11,3 +11,15 @@ window.addEventListener("load", () => {
   const enjoyText = document.querySelector(".decore img");
   enjoyText.classList.add("animate");
 });
+
+
+function validateEmail() {
+  const email = document.getElementById("email").value;
+  const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  
+  if (regex.test(email)) {
+    alert("Email is valid!");
+  } else {
+    alert("Please enter a valid email address.");
+  }
+}
